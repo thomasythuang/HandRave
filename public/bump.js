253e3b00f886b632c8118b87d5f95547a7064bdf
thomasythuang <thomas.yt.huang@gmail.com>
2014-11-22 19:54:27 -0600

    add node server & more methods

---
+++ public/bump.js
@@ -0,0 +1,112 @@
+window.plotter = new LeapDataPlotter({
+	el: document.getElementById('plots')
+});
+
+var output = document.getElementById('output'),
+	progress = document.getElementById('progress'),
+	display = document.getElementById('main');
+var audio = new Audio('resources/sounds/bump2.wav');
+var hit = false; 
+var posX;
+
+Leap.loop({background: true}, function(frame){
+    var hand = frame.hands[0];
+    if (!hand) return;
+    var xPosition = hand.palmPosition[0];
+    var height = hand.palmPosition[1];
+    var velocity = hand.palmVelocity[1];
+    
+  	output.innerHTML = hand.grabStrength.toPrecision(2);
+		progress.style.width = hand.grabStrength*100 + '%'; //hand.grabStrength * 100 + '%';
+
+		if (height < 150 && !hit){
+			hit = true;
+			bump(hit);
+		}else if(height > 150 && hit){
+			hit = false;
+			bump(hit);
+		}
+
+    if (posX == undefined){
+      if (xPosition >= 0)
+        posX = true;
+      else
+        posX = false;  
+    }
+
+    if (xPosition < 0 && posX){
+      posX = false;
+      cross();
+    }else if(xPosition >= 0 && !posX){
+      posX = true;
+      cross();
+    }
+
+    // call this once per frame per plot
+    plotter.plot('x position', xPosition, {
+      precision: 3,
+      units: 'mm'
+    });
+
+    // call this once per frame per plot
+    plotter.plot('height', height, {
+      precision: 3,
+      units: 'mm'
+    });
+
+    plotter.plot('y velocity', velocity, {
+      precision: 4,
+      units: 'mm/s'
+    });
+
+    // call this once per frame
+    plotter.update()
+  });
+
+function cross(){
+  audio.play();
+}
+
+function bump(hit){
+	if (hit){
+		display.style.backgroundColor = "#333333";
+		audio.play();
+	}else{
+		display.style.backgroundColor = "#ffffff";
+	}
+}
+
+// Adds the rigged hand plugin to the controller
+visualizeHand = function(controller){
+
+  controller.use('playback').on('riggedHand.meshAdded', function(handMesh, leapHand){
+	  handMesh.material.opacity = 0.8;
+  });
+  
+  var overlay = controller.plugins.playback.player.overlay;
+  overlay.style.right = 0;
+  overlay.style.left = 'auto';
+  overlay.style.top = 'auto';
+  overlay.style.padding = 0;
+  overlay.style.bottom = '13px';
+  overlay.style.width = '180px';
+
+
+  controller.use('riggedHand', { 
+		scale: 1.3,
+		boneColors: function (boneMesh, leapHand){
+		  if (boneMesh.name.indexOf('Finger_') == 0){
+				return {
+				  hue: 0.75,
+				  saturation: leapHand.grabStrength,
+				  lightness: 0.5
+				}
+		  } 
+		} 
+  });
+  
+  var camera = controller.plugins.riggedHand.camera;  
+  camera.position.set(0,20,-25);
+  camera.lookAt(new THREE.Vector3(0,3,0));
+};
+visualizeHand(Leap.loopController);
---
+++ public/sc.js
@@ -0,0 +1,19 @@
+SC.initialize({
+  client_id: '42913cd5bdf32a8069290ef75157ca08'
+});
+
+var widget = document.getElementById('widget');
+
+var track_url = 'https://soundcloud.com/steveaoki/steve-aoki-chris-lake-tujamo-delirious-boneless-feat-kid-ink';
+//var track_url = 'https://soundcloud.com/dodgeandfuski/dodge-fuski-call-my-name';
+
+SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+  console.log('oEmbed response: ' + oEmbed);
+  console.log(oEmbed);
+  widget.innerHTML = oEmbed.html;
+}); 
+
+/*
+SC.stream("/tracks/293", function(sound){
+  sound.play();
+});  */
